6aa351a92d8920ef50d3abb43ab32332b916689d
cytobi <tobi81548@gmail.com>
2023-09-27 21:44:34 +0200

    basic app layout

--- frontend/sailor_flutter/test/widget_test.dart
+++ frontend/sailor_flutter/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sailor_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SailorApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
